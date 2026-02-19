89c22c0cb9909c7bbebf312af4838d4f50381d0a
ZaydAyasta <81496269+ZaydAyasta@users.noreply.github.com>
2026-02-18 22:38:51 -0500

    ctrl+z

--- Assets/Characters/Player/Scripts/PlayerInputHandler.cs
+++ Assets/Characters/Player/Scripts/PlayerInputHandler.cs
@@ -6,30 +6,46 @@ public class PlayerInputHandler : MonoBehaviour
     public Vector2 Move { get; private set; }
     public bool JumpPressed { get; private set; }
     public bool RunHeld { get; private set; }
+
+    public bool GrabPressed { get; private set; }
+    public bool GrabHeld { get; private set; }
+
     public bool CrouchHeld { get; private set; }
 
     public void OnMove(InputAction.CallbackContext ctx)
-        => Move = ctx.ReadValue<Vector2>();
+    {
+        Move = ctx.ReadValue<Vector2>();
+    }
 
     public void OnJump(InputAction.CallbackContext ctx)
     {
         if (ctx.performed)
+        {
             JumpPressed = true;
+        }
     }
 
     public void OnRun(InputAction.CallbackContext ctx)
-        => RunHeld = ctx.ReadValueAsButton();
+    {
+        RunHeld = ctx.ReadValueAsButton();
+    }
+
+    public void OnGrab(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)
+            GrabPressed = true;
+
+        GrabHeld = ctx.ReadValueAsButton();
+    }
 
     public void OnCrouch(InputAction.CallbackContext ctx)
     {
-        if (ctx.performed)
-            CrouchHeld = true;
-        else if (ctx.canceled)
-            CrouchHeld = false;
+        CrouchHeld = ctx.ReadValueAsButton();
     }
 
     private void LateUpdate()
     {
         JumpPressed = false;
+        GrabPressed = false;
     }
 }
